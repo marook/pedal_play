95bba172c8b24edf1687965116d5da412fd1d761
Markus Peröbner <markus.peroebner@gmail.com>
2019-10-19 11:51:16 +0200

    add progress bar

--- pedal_play.py
+++ pedal_play.py
@@ -18,6 +18,7 @@
 #
 
 import os
+import progressbar
 import time
 import vlc
 
@@ -31,13 +32,18 @@ def play_file(file_path):
     p = vlc.MediaPlayer('file://{}'.format(file_abs_path))
     p.play()
     print('Ready to play')
-    while p.get_time() <= p.get_length():
-        last_play_request = play_file_age()
-        if last_play_request is None or last_play_request > 0.3:
-            p.set_pause(True)
-        else:
-            p.set_pause(False)
-        time.sleep(0.1)
+    bar = progressbar.ProgressBar().start()
+    try:
+        while p.get_time() <= p.get_length():
+            last_play_request = play_file_age()
+            if last_play_request is None or last_play_request > 0.3:
+                p.set_pause(True)
+            else:
+                p.set_pause(False)
+            time.sleep(0.1)
+            bar.update(min(100, 100*p.get_time() / p.get_length()))
+    finally:
+        bar.finish()
     
 def play_file_age():
     try:
